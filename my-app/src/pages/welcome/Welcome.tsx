db8e5bf45b53038b4f7ed1747361ec2a6e29bc5f
arossaadhikary <148636324+arossaadhikary@users.noreply.github.com>
2024-09-28 13:53:13 -0400

    change: home x welcome

--- my-app/src/pages/welcome/Welcome.tsx
+++ my-app/src/pages/welcome/Welcome.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './Welcome.css';
 
 const Welcome = () => {
+  const text = "Clarifina";
+
   return (
-    <div>Welcome</div>
-  )
-}
+    <section className="welcome_container">
+      <h1>
+        Hello. Welcome to{' '}
+        {text.split('').map((letter, index) => (
+          <span key={index} className="title_animation">
+            {letter}
+          </span>
+        ))}
+        .
+      </h1>
+    </section>
+  );
+};
 
 export default Welcome;
