44186816410f8f4cb9bca5577ba542c4b0b746f1
arossaadhikary <148636324+arossaadhikary@users.noreply.github.com>
2024-09-28 19:18:53 -0400

    change: updates to doc pg

--- my-app/src/pages/document_reader/Document.tsx
+++ my-app/src/pages/document_reader/Document.tsx
@@ -1,41 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Document.css';
 
 import logo from '../../assets/purple_logo.png';
 import sample from '../../assets/samplePdf.png';
 
 const Document = () => {
+  const [isPulsating, setIsPulsating] = useState(false);
+
+  useEffect(() => {
+    setIsPulsating(true);
+
+    const timer = setTimeout(() => {
+      setIsPulsating(false);
+    }, 3000);
+
+    return () => clearTimeout(timer); 
+  }, []);
+
   return (
     <section className="document_container">
-       <a href="/">
+      <a href="/">
         <img src={logo} className="document_logo" alt="Logo" />
       </a>
 
-      <p>Instructions: Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p>Instructions: Lorem Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum has been the industry's standard dummy text ever since the 1500 Lorem Ipsum has been the industry's standard dummy text ever since the 1500 Lorem Ipsum has been the industry's standard dummy text ever since the 1500...</p>
       
-      <div className = "document_button_container">
-        <button className = "button_normal">Word Selection</button>
-        <button className = "button_normal">Paragraph</button>
-        <button className = "button_suggestion">
-          <span className = "text">Our Suggestions</span>
+      <div className="document_button_container">
+        <button className="button_normal">Word Selection</button>
+        <button className="button_normal">Paragraph</button>
+        <button className={`button_special ${isPulsating ? 'pulsate' : ''}`}>
+          <span className="text">Our Suggestions</span>
+          <span className="shimmer"></span>
         </button>
-
-        
       </div>
 
       <div className="document_content">
         <div className="pdf_container">
           <iframe 
-            src={sample} // Replace with your PDF URL
+            src={sample} 
             width="100%" 
-            height="600px" // Adjust height as necessary
+            height="600px"
             style={{ border: 'none' }}
             title="PDF Document"
           ></iframe>
         </div>
 
         <div className="detail_container">
-          these are details
+          <div>
+            <h1>Document Analysis</h1>
+          </div>
+          <div className = "detail_content">
+            <div className = "detail_content_inside">
+              <h2 className = ""><mark>poindexter</mark></h2>
+            </div>
+          </div>
         </div>
       </div>
     </section>
