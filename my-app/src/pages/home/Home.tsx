08e335c31a7ecae9d2d8c7644a6e01dc535d708e
arossaadhikary <148636324+arossaadhikary@users.noreply.github.com>
2024-09-28 12:59:39 -0400

    add: home pg

--- my-app/src/pages/home/Home.tsx
+++ my-app/src/pages/home/Home.tsx
@@ -1,12 +1,43 @@
 import React from 'react'
+import './Home.css';
+
+import purpleLogo from '../../assets/purple_logo.png';
 
 const Home = () => {
   return (
-    <div>
-      Home
-
-    <p>hi</p>
-    </div>
+    <section className = "home_container animated_background">
+      <div className = "home_content">
+        <div className = "home_title">
+        <img src = {purpleLogo} className = "home_logo"/>
+          <h2>Let's read financial documents together.</h2>
+        </div>
+        
+        <div className="home_login">
+            <h2>Login</h2>
+            <form>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        required
+                    />
+                </div>
+                <button type="submit">Login</button>
+                <p>Don't have an account? Sign up.</p>
+            </form>
+        </div>
+      </div>
+      
+    </section>
     
   )
 }
