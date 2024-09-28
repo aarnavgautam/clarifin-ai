ea86c3213842d2637c784911757a0a8346d687ad
arossaadhikary <148636324+arossaadhikary@users.noreply.github.com>
2024-09-28 13:46:32 -0400

    change: home pg

--- my-app/src/pages/home/Home.tsx
+++ my-app/src/pages/home/Home.tsx
@@ -1,45 +1,34 @@
-import React from 'react'
+import React from 'react';
 import './Home.css';
-
 import purpleLogo from '../../assets/purple_logo.png';
 
 const Home = () => {
   return (
-    <section className = "home_container animated_background">
-      <div className = "home_content">
-        <div className = "home_title">
-        <img src = {purpleLogo} className = "home_logo"/>
-          <h2>Let's read financial documents together.</h2>
+    <section className="home_container animated_background">
+      <div className="home_content">
+        <div className="home_title">
+          <img src={purpleLogo} className="home_logo" alt="Logo" />
+          <h2>Clarifying financial documents, together.</h2>
         </div>
-        
+
         <div className="home_login">
-            <h2>Login</h2>
-            <form>
-                <div>
-                    <label htmlFor="username">Username:</label>
-                    <input
-                        type="text"
-                        id="username"
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        required
-                    />
-                </div>
-                <button type="submit">Login</button>
-                <p>Don't have an account? Sign up.</p>
-            </form>
+          <h2>Login</h2>
+          <form>
+            <div>
+              <label htmlFor="username">Username:</label>
+              <input type="text" id="username" required />
+            </div>
+            <div className="home_option">
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" required />
+            </div>
+            <button type="submit">Login</button>
+            <p>Don't have an account? Sign up.</p>
+          </form>
         </div>
       </div>
-      
     </section>
-    
-  )
+  );
 }
 
-export default Home
+export default Home;
