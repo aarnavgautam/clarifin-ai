a964977cf894b152d5438b3846138904af2d158e
arossaadhikary <148636324+arossaadhikary@users.noreply.github.com>
2024-09-28 14:32:58 -0400

    add: hello, welcome section

--- my-app/src/pages/welcome/Welcome.tsx
+++ my-app/src/pages/welcome/Welcome.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Welcome.css';
 
 const Welcome = () => {
-  const text = "Clarifina";
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowParagraph(true);
+    }, 2000);  // 3000 milliseconds = 3 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <section className="welcome_container">
-      <h1>
-        Hello. Welcome to{' '}
-        {text.split('').map((letter, index) => (
-          <span key={index} className="title_animation">
-            {letter}
-          </span>
-        ))}
-        .
-      </h1>
+      <h1>Hello. Welcome to Clarifina.</h1>
+      {showParagraph && (
+        <p className="fade-in">
+          Reading financial documents has never been this easy.
+        </p>
+      )}
     </section>
   );
 };
