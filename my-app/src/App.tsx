26db677d9abda7d70e629b2f1401a9e2561a8314
Burge <burgerito78@gmail.com>
2024-09-28 12:06:43 -0400

    Fixed the dependency problem

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 
-function App() {
+// importing pages
+import Home from './components/home/Home';
+import Welcome from './components/welcome/Welcome';
+import UserInformation from './components/user_information/User';
+import Profile from './components/profile/Profile';
+import Document from './components/document_reader/Document';
+
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/user-information" element={<UserInformation />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/document" element={<Document />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
